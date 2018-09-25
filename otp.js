64a00f289b1056f7f0d556f5e03ece61ef709c94
Enrique Santos <enriquesl@gmail.com>
2018-09-25 05:54:24 +0200

    First demo version of Blockchain authenticator
    A Blockchain-based OTP algorithm is shown, which is a modification of
    Time-based OTP. 5 blockchain networks can be used through th BlockCypher
    API. SHA-3 is used for HMAC.

--- otp.js
+++ otp.js
@@ -1,62 +1,79 @@
-/* TOTP (Authenticator demo) */
+/* BOTP (Blockchain authenticator demo) */
 
-var timeStep = 30;
-
+  
 $(function () {
     updateOtp();
 
-    $('#secret').keyup(function () {
+    $('#update').click(function () {
         updateOtp();
     });
-    
-    setInterval(timer, 1000);
+
+    $('#blockchain').change(function () {
+        updateOtp();
+    });
+
+    // setInterval(timer, 5000);
 });
+
+var timeStep = 10;
 
 function timer() {
     var epoch = Math.round(new Date().getTime() / 1000.0);
     var countDown = timeStep - (epoch % timeStep);
-    if (countDown == timeStep) updateOtp();
-    $('#updatingIn').text(countDown);
+    if (countDown == timeStep) updateOtp();    
 }
 
 function updateOtp() {
         
-    var key = base32tohex($('#secret').val());
-    var epoch = Math.round(new Date().getTime() / 1000.0);
-    var time = leftpad(dec2hex(Math.floor(epoch / timeStep)), 16, '0');
+    var key = base32tohex( $('#secret').val() );
+    var configURI = 'otpauth://botp/' + $('#userAtServer').val() + '?secret=' + $('#secret').val();
 
-    $('#qrImg').attr('src', 'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=200x200&chld=M|0&cht=qr&chl=otpauth://totp/user@host.com%3Fsecret%3D' + $('#secret').val());
     $('#secretHex').text(key);
     $('#secretHexLength').text(key.length * 4 + ' bits'); 
-    $('#epoch').text(time);
+    $('#configURI').text(configURI);
+    $('#qrImg').attr('src', 
+        'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=200x200&chld=M|0&cht=qr&chl=' 
+        + configURI);
     $('#hmac').empty();
 
     // updated for jsSHA v2.0.0 - http://caligatio.github.io/jsSHA/
-    var shaObj = new jsSHA("SHA-256", "HEX");
-    shaObj.setHMACKey(key, "HEX");
-    shaObj.update(time);
+    var shaObj = new jsSHA("SHA3-256", "HEX");  // SHA-3 used (keccak)
+    try {
+        shaObj.setHMACKey(key, "HEX");
 
-    var hmac = shaObj.getHMAC("HEX");
-    if (hmac == 'KEY MUST BE IN BYTE INCREMENTS') {
-        labelAppend('important', hmac);
-
-    } else {
-        // offset is the value of the last hex digit (half byte) of hmac
-        var offset = hex2dec(hmac.substring(hmac.length - 1));
+        var blockchain = $('#blockchain').val();
+        console.log(blockchain);
         
-        var part1 = hmac.substr(0, offset * 2);
-        if (part1.length > 0 )
+        $.get( "https://api.blockcypher.com/v1/" + blockchain + "/main", function( result ) {
+            var latestBlock = JSON.parse(result);
+            
+            console.log(latestBlock.hash);
+            $('#blockhash').text('0x' + latestBlock.hash);
+            shaObj.update(latestBlock.hash);
+
+            $('#blockcount').text(latestBlock.height);
+
+            var hmac = shaObj.getHMAC("HEX");
+                
+            // offset is the value of the last hex digit (half byte) of hmac
+            var offset = hex2dec(hmac.substring(hmac.length - 1));
+            
+            var part1 = hmac.substr(0, offset * 2);
+            var part2 = hmac.substr(offset * 2, 8);
+            var part3 = hmac.substr(offset * 2 + 8, hmac.length - offset);
+
             labelAppend('label-default', part1);
-
-        var part2 = hmac.substr(offset * 2, 8);
-        labelAppend('label-primary', part2);
-
-        var part3 = hmac.substr(offset * 2 + 8, hmac.length - offset);
-        if (part3.length > 0)
+            labelAppend('label-primary', part2);
             labelAppend('label-default', part3);
 
-        // var otp = hex2dec(part2);
-        $('#otp').text(hex2dec(part2) % 1000000);
+            $('#otp').text(hex2dec(part2) % 1000000);
+            
+        }, "text");
+    }
+    catch(err) {
+        labelAppend('label-primary', err.message);
+        $('#otp').text("");
+        console.log(err.message);
     }
 }
 
